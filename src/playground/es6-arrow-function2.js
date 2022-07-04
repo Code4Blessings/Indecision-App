d0827e9361b85995c11537a18b8dc9260d085a77
Code4Blessings <robincw35@gmail.com>
2022-07-04 09:03:02 -0400

    Created an arrow function2 file

--- src/playground/es6-arrow-function2.js
+++ src/playground/es6-arrow-function2.js
@@ -1,32 +1,28 @@
-'use strict';
-
 //Arguments object is no longer bound with arrow functions
 
 
-var add = function add(a, b) {
+const add = (a,b) => {
     //console.log(arguments)
     return a + b;
-};
+}
 
-console.log(add(55, 1));
+console.log(add(55,1))
 
 //this keyword is no longer bound with the function keyword but it is bound to arrow functions but can arrow functions do not nind it's own this value. 
 
-var user = {
+const user = {
     name: 'Hermione',
     cities: ['Coram', 'Richmond', 'Tamarac', 'Lawrenceville'],
-    printPlacesLived: function printPlacesLived() {
-        var _this = this;
-
-        this.name;
-        console.log(this.name);
-        this.cities;
-        console.log(this.cities);
+    printPlacesLived: function () {
+        this.name
+        console.log(this.name)
+        this.cities
+        console.log(this.cities)
         //the this keyword is not accessible in the below anonymous function. B
-        this.cities.forEach(function (city) {
-            console.log(_this.name + ' has lived in ' + city);
-        });
+        this.cities.forEach((city) => {
+            console.log(this.name + ' has lived in ' + city)
+        })
     }
-};
+}
 
-user.printPlacesLived();
+user.printPlacesLived()
